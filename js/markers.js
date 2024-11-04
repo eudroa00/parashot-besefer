41eafea22cd618bbfb202bcda59fac2ad89a43b3
eudroa00 <109921349+eudroa00@users.noreply.github.com>
2024-11-04 17:38:13 +0200

    update with potential solution to placement of numbers

--- js/markers.js
+++ js/markers.js
@@ -38,17 +38,11 @@ function placeVerses() {
     // Calculate the position relative to the index column
     const topPosition = markerRect.top - columnOffset;
 
-    // Set the top position for the number element
-    if (marker.classList.contains('inside')) {
-      if (window.innerWidth <= 576) {
-        numberElement.style.top = `${topPosition}px`;
-      } else {
-        numberElement.style.top = `${topPosition + 9}px`;
-      }
+    if (window.innerWidth <= 576) {
+      numberElement.style.top = `${topPosition + 4}px`;
     } else {
-      numberElement.style.top = `${topPosition + 1}px`;
+      numberElement.style.top = `${topPosition + 9}px`;
     }
-
     numberColumn.appendChild(numberElement);
   });
 
