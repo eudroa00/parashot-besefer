ba96b8d13c6f32149270d9dbb18f9edbccc386f6
eudroa00 <109921349+eudroa00@users.noreply.github.com>
2024-12-08 13:53:53 +0200

    Added Vayetze

--- js/text-to-html.js
+++ js/text-to-html.js
@@ -9,8 +9,7 @@ let r_q = '<div class="r-q">'; // %
 let end = '</div>'; // &
 let uTag = '<u>'; // \
 let u_end = '</u>'; // +
-let marker = '<span id="X_Y" class="marker">'; // ¡
-let close_marker = '</span>'; // !
+let close_marker = '</span>'; // Z | Opening marker is created in the function createVerseSpan()
 let small_space = '<div class="small-space"></div>';
 
 fs.readFile('../input.txt', 'utf8', (err, data) => {
@@ -35,11 +34,11 @@ fs.readFile('../input.txt', 'utf8', (err, data) => {
       data = changeThisFor(data, i, u_end);
     } else if (data[i] === '&') {
       data = changeThisFor(data, i, end);
-    } else if (data[i] === '1') {
-      data = changeThisFor(data, i, marker);
-    } else if (data[i] === '2') {
+    } else if (data[i] === 'R') {
+      data = createVerseSpan(data, i);
+    } else if (data[i] === `Z`) {
       data = changeThisFor(data, i, close_marker);
-    } else if (data[i] === '0') {
+    } else if (data[i] === 'S') {
       data = changeThisFor(data, i, small_space);
     }
     i++;
@@ -54,5 +53,26 @@ function changeThisFor(data, i, that) {
   let slice1 = data.slice(0, i);
   let slice2 = data.slice(i + 1, data.length);
   data = slice1 + that + slice2;
+  return data;
+}
+
+function createVerseSpan(data, i) {
+  let verse = '';
+  let verseDigits = 0;
+
+  if (i + 1 < data.length && !isNaN(data[i + 1])) {
+    verse += data[i + 1];
+    verseDigits++;
+  }
+  if (i + 2 < data.length && !isNaN(data[i + 2])) {
+    verse += data[i + 2];
+    verseDigits++;
+  }
+
+  let openingMarker = `<span id="X_${verse}" class="marker">`;
+
+  let slice1 = data.slice(0, i);
+  let slice2 = data.slice(i + verseDigits + 1, data.length);
+  data = slice1 + openingMarker + slice2;
   return data;
 }
